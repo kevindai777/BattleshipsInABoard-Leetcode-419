6b89586901734e440cf431f7f1ff3affd281878a
kevindai777 <kevin.dai.2451@gmail.com>
2020-07-11 12:17:54 -0400

    solutions

---
+++ battleshipsinaboard.js
@@ -0,0 +1,23 @@
+//Objective is to count the number of battleships on a board
+
+
+//O(nm) solution that checks each tile and it's sides
+
+let count = 0
+    
+for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+        count += isShip(board, i, j) ? 1 : 0
+    }
+}
+
+return count
+
+//Check if the left and up sides are empty
+function isShip(board, i, j) {
+    if (board[i][j] == 'X') {
+        return (i == 0 || board[i - 1][j] == '.') && (j == 0 || board[i][j - 1] == '.')
+    }
+    
+    return false
+}
